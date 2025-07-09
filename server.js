1aeaf62bb7e8485aacd51ca9f31378859e542f21
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-07-09 12:03:34 -0400

    Routing and the req object

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ import http from 'node:http'
 
 const PORT = 8000
 
+
 const server = http.createServer((req, res) => {
-    res.end('Hello World from the server!')
+    /*
+    Challenge: 
+    1. Check the ‘method’ property on the req object.
+       Only serve our string if it’s ‘GET’.
+    */
+
+
+    if (req.url === '/api' && req.method === 'GET') {
+        res.end('This is from the server')
+    }
 
 })
 
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
