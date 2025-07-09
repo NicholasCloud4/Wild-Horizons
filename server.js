bf951caf5c92c5535462aa71021202240c46aadd
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-07-09 11:30:08 -0400

    Created the server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('Hello World')
+import http from 'node:http'
+
+const PORT = 8000
+
+const server = http.createServer((req, res) => {
+    res.end('Hello World from the server!')
+
+})
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
